741bba3efae48a51d1f51594407b1bd3a5fcb9b4
xinqiyang <xinqiyang@gmail.com>
2012-08-30 23:56:33 +0800

    test common ok

--- library/common/common.lua
+++ library/common/common.lua
@@ -1,6 +1,16 @@
-ngx.say("hello nginx");
+module('common.common', package.seeall)
 
 
+function dofunction()
+	ngx.say("hello do function nginx");
+
+end
+
+
+function donewfunction()
+	ngx.say("hello do function nginx do new functions");
+
+end
 
 
 
